7c5b3bb4b6656f3832e18ca4feb3a5bf76d25825
Ryoneme2 <worknarair6@gmail.com>
2022-06-01 23:17:22 +0700

    Menu header

--- components/Section2.tsx
+++ components/Section2.tsx
@@ -10,11 +10,11 @@ const Section2 = (props: Props) => {
         <div className="block w-[100%] h-full relative">
           <img src="/images/waragamoset4K.png" alt="waragamoset image" />
           <div className='hidden lg:block'>
-            <div className='absolute w-[100%] h-[9rem] z-10 bottom-0 blur-sm'>
+            <div className='absolute w-[100%] h-[9rem] z-10 bottom-0'>
               <ImageWithLazyFill source='/svg/wave.svg' />
             </div>
             <div className='absolute w-[100%] h-[9rem] z-10 bottom-0 blur-lg'>
-              <ImageWithLazyFill source='/svg/wave.svg' />
+              <ImageWithLazyFill source='/svg/wave.svg'  />
             </div>
           </div>
         </div>
